edcf69e9731311504a0727fb4c4bd52abd84c222
AbhinRustagi <rustagi.abhin@gmail.com>
2023-03-27 01:22:44 +0530

    feat: add absolute imports

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
+import "@fontsource/barlow-semi-condensed";
+import "@fontsource/barlow-semi-condensed/300.css";
+import "@fontsource/barlow-semi-condensed/500.css";
+import "@fontsource/barlow-semi-condensed/600.css";
+import "@fontsource/barlow-semi-condensed/700.css";
+import "@fontsource/poppins";
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./styles/index.scss";
+import "src/styles/index.scss";
 
-import AdminLogin from "./admin/AdminLogin";
-import Logistics from "./admin/Logistics";
-import Companies from "./companies/companiespage";
-import CompanyPage from "./company-grid/CompanyPage";
-import Grid from "./company-grid/Grid";
-import Footer from "./components/footer/Footer";
-import Help from "./Help/Help";
-import Home from "./home/Home";
-import Login from "./login/login";
-import Profile from "./profile/profile";
-import RegistrationForm from "./register/RegistrationForm";
-import "./register/RegistrationForm.css";
+import AdminLogin from "src/admin/AdminLogin";
+import Logistics from "src/admin/Logistics";
+import Companies from "src/companies/companiespage";
+import CompanyPage from "src/company-grid/CompanyPage";
+import Grid from "src/company-grid/Grid";
+import Footer from "src/components/footer/Footer";
+import Help from "src/Help/Help";
+import Home from "src/home/Home";
+import Login from "src/login/login";
+import Profile from "src/profile/profile";
+import RegistrationForm from "src/register/RegistrationForm";
+import "src/register/RegistrationForm.css";
 
 function App() {
   return (
